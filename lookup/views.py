7680e71ff096e1cd9fb36583d3df13a6a14947de
Reza Ghorbani <rezaengplus@gmail.com>
2025-07-08 15:55:34 +0330

    added url from airnow website

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, 'home.html', {})
+	import json
+	import requests
+
+	api_request = requests.get("https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=89129&date=2025-07-08&distance=5&API_KEY=2764329E-90D0-43B0-B945-3D247C4175E2")
+	try:
+		api = json.loads(api_request.content)
+
+	except Exception as e:
+		api = "Error..."
+
+	return render(request, 'home.html', {'api': api})
 
 def about(request):
 	return render(request, 'about.html', {})
